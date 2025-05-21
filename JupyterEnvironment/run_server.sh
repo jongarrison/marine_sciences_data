801a76bf8e9cd79a008d0eae44b3294522b17094
JonG <jongarrison@gmail.com>
2025-05-21 15:59:30 -0700

    making the jupyter script clearer

--- JupyterEnvironment/run_server.sh
+++ JupyterEnvironment/run_server.sh
@@ -5,10 +5,15 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 fi
 
 if [[ -z "$VIRTUAL_ENV" ]]; then
-    echo "No virtual environment detected. Creating a new one..."
+    echo "No virtual environment detected. Loading the local .env..."
     
     # Activate the virtual environment
     source .env/bin/activate
 fi
+
+echo "========================="
+echo "Find the Jupyter servert at:"
+echo "http://127.0.0.1:8888/tree?token=SuperSecretToken"
+echo "========================="
 
 jupyter notebook --NotebookApp.token=SuperSecretToken
